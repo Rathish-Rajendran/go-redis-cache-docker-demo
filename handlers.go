a89c42bff94a56d526a822073f77c49374914ea7
Rathish-Rajendran <rathishrajendran9601@gmail.com>
2025-12-26 20:55:44 +0530

    Adding cache layer

--- handlers.go
+++ handlers.go
@@ -3,11 +3,11 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/labstack/gommon/log"
 )
-
-
 
 func CreateUserHandler(w http.ResponseWriter, r *http.Request){
 	userData := User{}
@@ -23,15 +23,38 @@ func CreateUserHandler(w http.ResponseWriter, r *http.Request){
 func GetUserHandler(w http.ResponseWriter, r *http.Request){
 	id := mux.Vars(r)["id"]
 	userData := User{}
-	err := DB.QueryRow(r.Context(),
+	keyInRedis := "user:"+id
+
+	valFromRedis, err := redisClient.Get(r.Context(), keyInRedis).Result()
+	if err == nil {
+		json.Unmarshal([]byte(valFromRedis), &userData)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"source": "cache",
+			"data": userData,
+		})
+		return
+	}
+
+	err = DB.QueryRow(r.Context(),
 		"SELECT id, name, role FROM users WHERE id=$1",
 		id,
 	).Scan(&userData.ID, &userData.Name, &userData.Role)
-
 	if err != nil {
 		http.Error(w, "Error fetching user", http.StatusInternalServerError)
 		return
 	}
 
-	json.NewEncoder(w).Encode(userData)	
+	userDataInJsonBytes, err := json.Marshal(userData)
+	if err != nil {
+		log.Warnf("error json marshalling user data before inserting into redis for user id:%v", id)
+	}
+	err = redisClient.Set(r.Context(), keyInRedis, userDataInJsonBytes, 30*time.Second).Err()
+	if err != nil {
+		log.Warnf("error inserting data to redis for user id:%v", id)
+	}
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"source": "database",
+		"data": userData,
+	})
 }
